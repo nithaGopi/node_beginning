d1fca4caa4213b9076e71e98479768fd749194f2
nithaGopi <102636295+nithaGopi@users.noreply.github.com>
2022-05-18 15:17:44 +0530

    Add files via upload
    Asynchronous functions  - js

---
+++ syncfunc.js
@@ -0,0 +1,14 @@
+function showEnd() {
+
+console.log("END")
+
+
+}
+console.log("started")
+setTimeout(showEnd,2000) // this is an asynchronous function
+
+console.log("started")
+setTimeout(showEnd,2000)
+
+console.log("started")
+setTimeout(showEnd,2000)
