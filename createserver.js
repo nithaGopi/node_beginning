969f588c241577f1c26e1f6567b251459888e670
nithaGopi <102636295+nithaGopi@users.noreply.github.com>
2022-05-19 10:33:39 +0530

    Add files via upload
    Add route in  createserver.js file

--- createserver.js
+++ createserver.js
@@ -11,13 +11,20 @@ var fs   = require('fs');
 //ADD HTML THROUGH WRITE()
 
 http.createServer(function (req, res) {
+    if(req.url==='/'){
+       fs.readFile('table.html',function(err,data){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
 
-   fs.readFile('table.html',function(err,data){
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-
-    })
+        })
+    } else if(req.url==='/login'){
+        res.write('login')
+        res.end()
+    } else{
+        res.write('error')
+        res.end()
+    }
 }).listen(7000);
 
 
